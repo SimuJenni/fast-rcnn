f079a29933b882fd4068abd0472155af99314698
SimuJenni <simujenni@gmail.com>
2017-05-15 21:49:05 +0200

    Trying another ToonNet setting

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -118,13 +118,13 @@ __C.DEDUP_BOXES = 1./16.
 # Pixel mean values (BGR order) as a (1, 1, 3) array
 # We use the same pixel mean for all networks even though it's not exactly what
 # they were trained with
-__C.PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+__C.PIXEL_MEANS = np.array([[[127.5, 127.5, 127.5]]])
 
 # For reproducibility
 __C.RNG_SEED = 3
 
 # A small number that's used many times
-__C.EPS = 1e-9
+__C.EPS = 1e-14
 
 # Root directory of project
 __C.ROOT_DIR = osp.abspath(osp.join(osp.dirname(__file__), '..', '..'))
